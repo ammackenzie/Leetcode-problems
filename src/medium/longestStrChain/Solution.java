2362ab32c0ed7105079b730694fda9d79af2ea58
Andrew Mackenzie <andrew_mackenzie@slc.co.uk>
2022-06-15 16:52:52 +0100

    added longestStrChain

--- src/medium/longestStrChain/Solution.java
+++ src/medium/longestStrChain/Solution.java
@@ -1,8 +1,23 @@
 package medium.longestStrChain;
 //https://leetcode.com/problems/longest-string-chain/
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Solution {
     public int longestStrChain(String[] words) {
-        return 0;
+        Arrays.sort(words, (a, b) -> a.length()-b.length());
+        HashMap<String, Integer> dp = new HashMap<>();
+        int maxCount = 0;
+        for(String word : words){
+            int currentCount = 0;
+            for(int i=0; i < word.length(); i++){
+                String previous = word.substring(0, i) + word.substring(i+1);
+                currentCount = Math.max(currentCount, dp.getOrDefault(previous, 0)+1);
+            }
+            dp.put(word, currentCount);
+            maxCount = Math.max(maxCount, currentCount);
+        }
+        return maxCount;
     }
 }
