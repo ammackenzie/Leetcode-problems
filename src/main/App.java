6ae97df45edc9b80bbbce2425137024610ed4f17
Andrew Mackenzie <andrew_mackenzie@slc.co.uk>
2022-09-09 11:41:48 +0100

    added numberOfWeakCharacters

--- src/main/App.java
+++ src/main/App.java
@@ -1,16 +1,19 @@
 package main;
 
 
-import medium.maxArea.Solution;
+
+import medium.numberOfWeakCharacters.Solution;
 
 import java.util.*;
 
 public class App {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        System.out.println(solution.maxArea(
-                1000000000,
-                1000000000, new int[]{2},new int[]{2}));
+        int[][] properties = {{1,5},{10,4},{4,3}, {10, 3}};
+        for(int i =0; i< properties.length; i++){
+            System.out.println(properties[i][0] + ", " + properties[i][1]);
+        }
+        System.out.println(solution.numberOfWeakCharacters(properties));
     }
 
 }
